7e525e1d88ba4b3afe5749a535b787193dd5c1b1
Ambuj Kumar <itsambuja@gmail.com>
2024-07-14 12:05:02 +0530

    find winner

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function Board(){
   function handleClick(i){
     const nextSquare = squares.slice();
 
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
       return;
     }
 
@@ -25,8 +25,17 @@ export default function Board(){
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={ () => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={ () => handleClick(1)} />
@@ -44,4 +53,24 @@ export default function Board(){
       </div>
     </>  
     );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
